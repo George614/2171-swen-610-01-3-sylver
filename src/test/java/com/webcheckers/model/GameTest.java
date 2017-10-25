762f1e5c87986590e9d94ddf8164bf661c98cac8
mfabbu <mfabbu@rit.edu>
2017-10-25 10:50:10 -0400

    Unit Test: GameTest done.

--- src/test/java/com/webcheckers/model/GameTest.java
+++ src/test/java/com/webcheckers/model/GameTest.java
@@ -1,44 +1,39 @@
 package com.webcheckers.model;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertSame;
-
-//import static org.junit.jupiter.api.Assertions.*;
-
 
 /**
  *  The unit test suite for the {@link Game} component.
  *
  *  @author <a href="mailto:mfabbu@rit.edu">Matt Arlauckas</a>
  */
-class GameTest {
+public class GameTest {
 
     private static final String PLAYER_RED_NAME = "Bob";
     private static final String PLAYER_WHITE_NAME = "Sally";
     private static final Player PLAYER_RED = new Player(PLAYER_RED_NAME);
     private static final Player PLAYER_WHITE = new Player(PLAYER_WHITE_NAME);
 
+    @Before
+    public void setUp() throws Exception {
+    }
 
-//    @Before
-//    void setUp() {
-//    }
-//
-//    @org.junit.jupiter.api.AfterEach
-//    void tearDown() {
-//    }
-
-//    @Test
-
+    @After
+    public void tearDown() throws Exception {
+    }
 
     @Test
-    void getPlayerRedUsername() {
+    public void getPlayerRedUsername() throws Exception {
         final Game CuT = new Game(PLAYER_RED,PLAYER_WHITE);
         assertSame(PLAYER_RED_NAME, PLAYER_RED.getUsername());
     }
 
     @Test
-    void getPlayerWhiteUsername() {
+    public void getPlayerWhiteUsername() throws Exception {
         final Game CuT = new Game(PLAYER_RED,PLAYER_WHITE);
         assertSame(PLAYER_WHITE_NAME, PLAYER_WHITE.getUsername());
     }
--- src/test/java/com/webcheckers/ui/GetGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetGameRouteTest.java
@@ -3,9 +3,6 @@ package com.webcheckers.ui;
 import com.webcheckers.appl.GameCenter;
 import com.webcheckers.model.Game;
 import com.webcheckers.model.Player;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
@@ -16,9 +13,7 @@ import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Session;
-
 import static org.junit.Assert.assertEquals;
-//import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -29,14 +24,7 @@ import static org.mockito.Mockito.when;
  *
  *  @author <a href="mailto:mfabbu@rit.edu">Matt Arlauckas</a>
  */
-class GetGameRouteTest {
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void handle() throws Exception {
-    }
+public class GetGameRouteTest {
 
     private static final String OPPONENT_COLOR = "Red";
     private static final String PLAYER_COLOR = "White";
@@ -50,51 +38,27 @@ class GetGameRouteTest {
      */
     private GetGameRoute CuT = new GetGameRoute(new GameCenter());
 
+    // Mock objects
     private Request request;
     private Session session;
-    private Response response;
+
 
     /**
      *  Set up mock objects for each test
      */
     @Before
-    void setUp() {
+    public void setUp() {
         request = mock(Request.class);
         session = mock(Session.class);
         when(request.session()).thenReturn(session);
-        response = mock(Response.class);
     }
 
-    /**
-     *  Test that the Game view will create a new game if none exists.
-     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
     @Test
-    public void new_game() {
-        // Arrange the test scenario; The session holds no game
-        final Game game = new Game(PLAYER_RED,PLAYER_WHITE);
-        when(session.attribute(GameCenter.GAME_ID)).thenReturn(game);
+    public void handleTest() throws Exception {
 
-        // Invoke the test
-        final ModelAndView result = CuT.handle(request, response);
-
-        // Analyze the results
-        //  * is not null
-        assertNotNull(result);
-
-        //  * model is a not null Map
-        final Object model = result.getModel();
-        assertNotNull(model);
-        assertTrue(model instanceof Map);
-
-        //  * model contains all necessary View-Model data
-        final Map<String, Object> vm = (Map<String, Object>) model;
-        assertEquals(GetGameRoute.TITLE, vm.get(GetGameRoute.TITLE_ATTR));
-        assertEquals(PLAYER_COLOR, vm.get(GetGameRoute.PLAYER_COLOR_ATTR));
-        assertEquals(OPPONENT_COLOR, vm.get(GetGameRoute.OPPONENT_COLOR_ATTR));
-
-        //  * test view name
-        assertEquals(GetGameRoute.VIEW_NAME, result.getViewName());
     }
-
-
 }
